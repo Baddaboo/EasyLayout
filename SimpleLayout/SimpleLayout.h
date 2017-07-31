0bf404383e2bd58f250d68f03b694551ae488ba0
Blake Tsuzaki <dtsuzaki@scu.edu>
2017-07-30 23:14:13 -1000

    Initial Commit

---
+++ SimpleLayout/SimpleLayout.h
@@ -0,0 +1,19 @@
+//
+//  SimpleLayout.h
+//  SimpleLayout
+//
+//  Created by Blake Tsuzaki on 7/30/17.
+//  Copyright © 2017 Modoki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleLayout.
+FOUNDATION_EXPORT double SimpleLayoutVersionNumber;
+
+//! Project version string for SimpleLayout.
+FOUNDATION_EXPORT const unsigned char SimpleLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleLayout/PublicHeader.h>
+
+
